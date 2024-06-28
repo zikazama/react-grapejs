526f14a75de944453275ff6861a98fe0afe75fa3
Fauzi <Fauzi190198@gmail.com>
2024-06-28 16:34:52 +0700

    feat: custom plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,37 @@
-
-import {useEffect} from "react";
+import { useEffect } from "react";
 
 import grapesjs from "grapesjs";
-import 'grapesjs/dist/css/grapes.min.css'
-import websitePlugin from 'grapesjs-preset-webpage';
-import basicBlockPlugin from 'grapesjs-blocks-basic'
-import formPlugin from 'grapesjs-plugin-forms'
-
+import "grapesjs/dist/css/grapes.min.css";
+import websitePlugin from "grapesjs-preset-webpage";
+import basicBlockPlugin from "grapesjs-blocks-basic";
+import formPlugin from "grapesjs-plugin-forms";
 
 function App() {
+  function CustomPlugin(editor: {
+    Blocks: {
+      add: (arg0: string, arg1: { label: string; content: string }) => void;
+    };
+  }) {
+    editor.Blocks.add("simple-block", {
+      label: "Simple block",
+      content: '<div class="my-block">Ini contoh simple block</div>',
+    });
 
-    useEffect(() => {
-       grapesjs.init({
-         container: '#gjs',
-         width: '100%',
-         plugins: [websitePlugin,basicBlockPlugin,formPlugin],
-       })
-    },[])
+    editor.Blocks.add("input-block", {
+      label: "Input block",
+      content: '<input value="ini contoh input"></input>',
+    });
+  }
 
-    return (
-        <div id="gjs"></div>
-    );
+  useEffect(() => {
+    grapesjs.init({
+      container: "#gjs",
+      width: "100%",
+      plugins: [websitePlugin, CustomPlugin],
+    });
+  }, []);
+
+  return <div id="gjs"></div>;
 }
 
 export default App;
-
--- src/components/GrapeEditor.tsx
+++ src/components/GrapeEditor.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef } from 'react';
-import grapesjs from 'grapesjs';
-import 'grapesjs/dist/css/grapes.min.css';
+import React, { useEffect, useRef } from "react";
+import grapesjs from "grapesjs";
+import "grapesjs/dist/css/grapes.min.css";
 
 const GrapeEditor: React.FC = () => {
   const editorRef = useRef<HTMLDivElement>(null);
@@ -10,28 +10,27 @@ const GrapeEditor: React.FC = () => {
       grapesjs.init({
         container: editorRef.current,
         fromElement: true,
-        width: 'auto',
-        height: '100vh',
+        width: "auto",
+        height: "100vh",
         storageManager: false,
         blockManager: {
-          appendTo: '#blocks',
+          appendTo: ".myblocks",
           blocks: [
             {
-              id: 'section',
-              label: '<b>Section</b>',
-              attributes: { class: 'gjs-block-section' },
-              content: `<section>
-                          <h1>This is a simple section</h1>
-                          <p>You can add more content here</p>
-                        </section>`
+              id: "image",
+              label: "Image",
+              media: `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+            <path d="M8.5,13.5L11,16.5L14.5,12L19,18H5M21,19V5C21,3.89 20.1,3 19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19Z" />
+        </svg>`,
+              // Use `image` component
+              content: { type: "image" },
+              // The component `image` is activatable (shows the Asset Manager).
+              // We want to activate it once dropped in the canvas.
+              activate: true,
+              // select: true, // Default with `activate: true`
             },
-            {
-              id: 'text',
-              label: 'Text',
-              content: '<div data-gjs-type="text">Insert your text here</div>'
-            }
-          ]
-        }
+          ],
+        },
       });
     }
   }, []);
