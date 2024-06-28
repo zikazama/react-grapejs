2b0b94d691bc933c6f3b16308b1c5b31100bb4fa
Fauzi <Fauzi190198@gmail.com>
2024-06-28 14:06:22 +0700

    fix: config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 80
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 80
+  },
   plugins: [react()],
 })
